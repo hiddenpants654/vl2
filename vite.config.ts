1f4e43e52fafe0969885421aeb23ef1f1f9cc42d
Hiddenpants654 <trent.woolsey@gmail.com>
2025-06-07 15:08:33 -0500

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vl2/', // <-- Add this line for GitHub Pages
   plugins: [react()],
 })
